41661d9afa757182682d6495c6b2a826f45dbad0
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2024-11-28 19:20:09 +0700

    update main

--- HandTracker/main.py
+++ HandTracker/main.py
@@ -14,12 +14,12 @@ hands = mp_hands.Hands() #mendeteksi tangan dalam gambar.
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
-    ret, image = cap.read() #cap.read(): Mengambil frame dari kamera. Jika gagal, mencetak pesan error dan keluar dari loop.
+    ret, image = cap.read()
     if not ret:
         print("Error: Failed to capture image.")
         break
 
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB) #cv2.flip(image, 1): Membalik gambar secara horizontal (mirror effect). 
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -28,21 +28,17 @@ while True:
             # Mendapatkan label tangan (kiri/kanan)
             hand_label = handedness.classification[0].label
             
-            #Mendapatkan posisi landmark pergelangan tangan (WRIST) untuk menentukan di mana label akan ditampilkan.
-            # Menampilkan label tangan di gambar
+            #Mendapatkan posisi landmark
+            #Menampilkan label tangan di gambar
             label_position = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
             h, w, _ = image.shape
 
             #Menghitung koordinat x dan y dari posisi tersebut berdasarkan ukuran gambar.
             cx, cy = int(label_position.x * w), int(label_position.y * h)
 
-            # Size text
             (text_width, text_height), _ = cv2.getTextSize(hand_label, font, 0.5, 2)
             # Calculate the x-coordinate to horizontally center the text
             x = int(cx - text_width / 2)
-
-            #Menampilkan label di atas pergelangan tangan dengan posisi yang sudah dihitung.
-            # Menggambar landmark tangan pada gambar menggunakan `mp_drawing.draw_landmarks
             cv2.putText(image, hand_label, (x, cy - -50), font, 0.5, (900, 255, 0), 2)
 
             # Menggambar landmark tangan
